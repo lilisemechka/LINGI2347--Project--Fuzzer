9e943033cae58a960a861e423118893d5c8cebfb
lilisemechka <liliya.semerikova@student.uclouvain.be>
2020-06-16 17:55:15 +0200

    reoder the code

--- fuzzer.py
+++ fuzzer.py
@@ -11,11 +11,36 @@ def random_string(stringLength=10):
     password_characters = string.ascii_letters + string.digits + string.punctuation
     return ''.join(random.choice(password_characters) for i in range(stringLength))
 
-def test_input_version():
+def lunch_process(type_of_test):
 
     crash = 0
+    
+    process = subprocess.Popen(["./converter", "newinput.img", "testoutput.img"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    error_string = ""
+    try:
+        for line in process.stdout: 
+            if "*** The program has crashed ***" in str(line):
+                crash = 1
+                print(line)
+           
+    except TimeoutExpired:
+        process.kill() 
+        out, err = process.communicate()
+        process.terminate()
 
-    for i in range(40):
+    if(crash == 1):
+        os.rename('newinput.img', type_of_test)
+    
+    return crash
+
+def test_input_version():
+
+    print("test_input_version")
+
+    bug = 1
+
+    while bug:
         
         file1 = open('testinput.img', 'rb')
         file2 = open('newinput.img', 'wb+')
@@ -23,7 +48,7 @@ def test_input_version():
         byte = file1.read(1)
         file2.write(byte)
 
-        a = random.randint(0, 200)
+        a = random.randint(0, 10000)
         byte_to_write = a.to_bytes(2,'little')
         count = 0
         while byte:
@@ -39,29 +64,17 @@ def test_input_version():
         file1.close()
         file2.close()
 
-        ## Shell=False helps the process terminate
-        process = subprocess.Popen(["./converter", "newinput.img", "testoutput.img"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    
-        ## Get exit codes
-        error_string = ""
-        try:
-            for line in process.stdout: 
-                if "*** The program has crashed ***" in str(line):
-                    crash = 1
-                    print(line)
-           
-        except TimeoutExpired:
-            process.kill() 
-            out, err = process.communicate()
-            process.terminate()
-
-        if(crash == 1):
-            os.rename('newinput.img', 'bad_version.img')
+        
+        if(lunch_process('bad_version.img')):
             break
 
 def test_comment_size():
 
-    for i in range(10):
+    print("test_comment_size")
+
+    bug = 1
+
+    while bug:
 
         file1 = open('testinput.img', 'rb')
         file2 = open('newinput.img', 'wb+')
@@ -70,8 +83,6 @@ def test_comment_size():
         file2.write(byte)
 
         a = random.randint(0, 10000)
-        print("Comment size")
-        print(a)
         random_comment = random_string(a)
         hex_string = str.encode(random_comment)
         while byte:
@@ -88,23 +99,12 @@ def test_comment_size():
         file1.close()
         file2.close()
 
-
-        ## Shell=False helps the process terminate
-        process = subprocess.Popen(["./converter", "newinput.img", "newoutput.img"], shell=False)
-    
-        ## Get exit codes
-        try:
-            out, err = process.communicate(timeout = 15)
-            errcode = process.returncode
-            if("The programe has crashed" in str(errcode)):
-                print(errcode)
-            print(errcode)
-        except TimeoutExpired:
-            process.kill() 
-            out, err = process.communicate()
-            process.terminate()
+        if(lunch_process('bad_comment_size.img')):
+            break
 
 def number_of_color():
+
+    print("number_of_color")
 
     for i in range(20):
 
@@ -130,25 +130,16 @@ def number_of_color():
         file1.close()
         file2.close()
 
-
-    ## Shell=False helps the process terminate
-        process = subprocess.Popen(["./converter", "newinput.img", "newoutput.img"], shell=False)
-    
-    ## Get exit codes
-        try:
-            out, err = process.communicate(timeout = 15)
-            errcode = process.returncode
-            if("The programe has crashed" in str(errcode)):
-                print(errcode)
-            print(errcode)
-        except TimeoutExpired:
-            process.kill() 
-            out, err = process.communicate()
-            process.terminate()
+        if(lunch_process('bad_number_of_color.img')):
+            break
 
 def test_name_size():
 
-    for i in range(10):
+    print("test_name_size")
+
+    bug = 1
+
+    while bug:
 
         file1 = open('testinput.img', 'rb')
 
@@ -157,7 +148,7 @@ def test_name_size():
         byte = file1.read(1)
         file2.write(byte)
 
-        a = random.randint(0, 100)
+        a = random.randint(0, 1000)
         random_comment = random_string(a)
         hex_string = str.encode(random_comment)
         while byte:
@@ -174,25 +165,16 @@ def test_name_size():
         file1.close()
         file2.close()
 
-
-        ## Shell=False helps the process terminate
-        process = subprocess.Popen(["./converter", "newinput.img", "newoutput.img"], shell=False)
-    
-        ## Get exit codes
-        try:
-            out, err = process.communicate(timeout = 15)
-            errcode = process.returncode
-            if("The programe has crashed" in str(errcode)):
-                print(errcode)
-            print(errcode)
-        except TimeoutExpired:
-            process.kill() 
-            out, err = process.communicate()
-            process.terminate()
+        if(lunch_process('bad_name_size.img')):
+            break
 
 def test_color_table():
 
-    for i in range(10):
+    print("test_color_table")
+
+    bug = 1
+
+    while bug:
 
         file1 = open('testinput.img', 'rb')
 
@@ -219,20 +201,8 @@ def test_color_table():
         file2.close()
 
 
-        ## Shell=False helps the process terminate
-        process = subprocess.Popen(["./converter", "newinput.img", "newoutput.img"], shell=False)
-    
-        ## Get exit codes
-        try:
-            out, err = process.communicate(timeout = 15)
-            errcode = process.returncode
-            if("The programe has crashed" in str(errcode)):
-                print(errcode)
-            print(errcode)
-        except TimeoutExpired:
-            process.kill() 
-            out, err = process.communicate()
-            process.terminate()
+        if(lunch_process('bad_color_table.img')):
+            break
 
 def test_width_height():
 
@@ -245,10 +215,6 @@ def test_width_height():
         byte = file1.read(1)
         file2.write(byte)
 
-        """a = random.randint(0, 300)
-        byte_to_write_a = a.to_bytes(8,'little')
-        b = random.randint(0, 300s)
-        byte_to_write_b = b.to_bytes(8,'little')"""
         random_comment_a = random_string(8)
         hex_string_a = str.encode(random_comment_a)
         a = random.randint(0, 2)
@@ -270,27 +236,15 @@ def test_width_height():
         file2.close()
 
 
-        ## Shell=False helps the process terminate
-        process = subprocess.Popen(["./converter", "newinput.img", "newoutput.img"], shell=False)
-    
-        ## Get exit codes
-        try:
-            out, err = process.communicate(timeout = 15)
-            errcode = process.returncode
-            if("The programe has crashed" in str(errcode)):
-                print(errcode)
-            print(errcode)
-        except TimeoutExpired:
-            process.kill() 
-            out, err = process.communicate()
-            process.terminate()
+        if(lunch_process('bad_color_table.img')):
+            break
 
 
 def test_no_io():
     test_input_version() # works and return the failure
-    """test_comment_size()""" # works and return the failure
-    """test_color_table()""" # works and return the failure
-    """test_name_size()""" # crashed very rarely Author: 09Ng *** The program has crashed *** mettre bcp de fois for 
+    test_comment_size()  # works and return the failure
+    test_color_table()   # works and return the failure
+    test_name_size()     # crashed very rarely Author: 09Ng *** The program has crashed *** mettre bcp de fois for 
     
     """test_width_height()""" # do not return the failure
     """number_of_color()""" # do not return the failure
