6ea622fea5c3a2f3ee1411c05dab1c034fe590c0
lilisemechka <liliya.semerikova@student.uclouvain.be>
2020-06-17 11:36:54 +0200

    5th bug found

--- fuzzer.py
+++ fuzzer.py
@@ -6,6 +6,7 @@ import string
 import codecs
 import binascii
 import os
+import time
 
 def random_string(stringLength=10):
     password_characters = string.ascii_letters + string.digits + string.punctuation
@@ -38,9 +39,10 @@ def test_input_version():
 
     print("test_input_version")
 
-    bug = 1
+    timeout = time.time() + 60
+    bug = 0
 
-    while bug:
+    while not bug:
         
         file1 = open('testinput.img', 'rb')
         file2 = open('newinput.img', 'wb+')
@@ -65,16 +67,22 @@ def test_input_version():
         file2.close()
 
         
-        if(lunch_process('bad_version.img')):
+        if lunch_process('bad_version.img'):
+            bug = 1
+
+        if time.time() > timeout:
+            print("Test has reached the timeout")
             break
+
 
 def test_comment_size():
 
     print("test_comment_size")
 
-    bug = 1
+    timeout = time.time() + 60
+    bug = 0
 
-    while bug:
+    while not bug:
 
         file1 = open('testinput.img', 'rb')
         file2 = open('newinput.img', 'wb+')
@@ -91,6 +99,7 @@ def test_comment_size():
                 file2.write(hex_string)
                 while byte != b'\x00':
                     byte = file1.read(1)
+
                 file2.write(byte)
             else:
                 byte = file1.read(1)
@@ -100,38 +109,12 @@ def test_comment_size():
         file2.close()
 
         if(lunch_process('bad_comment_size.img')):
+            bug = 1
+
+        if time.time() > timeout:
+            print("Test has reached the timeout")
             break
 
-def number_of_color():
-
-    print("number_of_color")
-
-    for i in range(20):
-
-        file1 = open('testinput.img', 'rb')
-
-        file2 = open('newinput.img', 'wb+')
-
-        byte = file1.read(1)
-        file2.write(byte)
-
-        a = random.randint(0, 300)
-        byte_to_write = a.to_bytes(5,'little')
-        while byte:
-            if(byte == b'\x0a'):
-                file2.write(byte)
-                byte = file1.read(4)
-                file2.write(byte_to_write)
-            else:
-                byte = file1.read(1)
-                file2.write(byte)
-
-
-        file1.close()
-        file2.close()
-
-        if(lunch_process('bad_number_of_color.img')):
-            break
 
 def test_name_size():
 
@@ -168,13 +151,43 @@ def test_name_size():
         if(lunch_process('bad_name_size.img')):
             break
 
-def test_color_table():
+def test_magic_number():
 
-    print("test_color_table")
+    print("test_magic_number")
 
     bug = 1
 
     while bug:
+
+        file1 = open('testinput.img', 'rb')
+
+        file2 = open('newinput.img', 'wb+')
+
+        byte = file1.read(4)
+        file2.write(byte)
+
+        a = random.randint(0, 100)
+        random_comment = random_string(a)
+        hex_string = str.encode(random_comment)
+        file2.write(hex_string)
+        while byte:
+            byte = file1.read(1)
+            file2.write(byte)
+
+        file1.close()
+        file2.close()
+
+        if(lunch_process('bad_name_size.img')):
+            break
+
+def test_color_table():
+
+    print("test_color_table")
+
+    timeout = time.time() + 60
+    bug = 0
+
+    while not bug:
 
         file1 = open('testinput.img', 'rb')
 
@@ -202,41 +215,9 @@ def test_color_table():
 
 
         if(lunch_process('bad_color_table.img')):
-            break
+            bug = 1
 
-def test_width_height():
-
-    for i in range(10):
-
-        file1 = open('testinput.img', 'rb')
-
-        file2 = open('newinput.img', 'wb+')
-
-        byte = file1.read(1)
-        file2.write(byte)
-
-        random_comment_a = random_string(8)
-        hex_string_a = str.encode(random_comment_a)
-        a = random.randint(0, 2)
-        random_comment_b = random_string(8)
-        hex_string_b = str.encode(random_comment_b)
-        while byte:
-            if(byte == b'\x02'):
-                byte = file1.read(8)
-                file2.write(hex_string_a)
-                byte = file1.read(8)
-                file2.write(hex_string_b)
-                byte = file1.read(8)
-                file2.write(byte)
-            else:
-                byte = file1.read(1)
-                file2.write(byte)
-
-        file1.close()
-        file2.close()
-
-
-        if(lunch_process('bad_color_table.img')):
+        if(lunch_process('bad_number_of_color.img')):
             break
 
 
@@ -244,9 +225,8 @@ def test_no_io():
     test_input_version() # works and return the failure
     test_comment_size()  # works and return the failure
     test_color_table()   # works and return the failure
-    test_name_size()     # crashed very rarely Author: 09Ng *** The program has crashed *** mettre bcp de fois for 
-    
-    """test_width_height()""" # do not return the failure
-    """number_of_color()""" # do not return the failure
+    test_name_size()     # works and return the failure
+    test_magic_number()  # works and return the failure
+
 
 test_no_io()
