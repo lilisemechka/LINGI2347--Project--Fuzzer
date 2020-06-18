a29ae0347d61d754bc8327a4f8d7b7b01ad32cc0
lilisemechka <liliya.semerikova@student.uclouvain.be>
2020-06-18 15:30:39 +0200

    all 6 crashes found

--- fuzzer.py
+++ fuzzer.py
@@ -35,7 +35,8 @@ def lunch_process(type_of_test):
     
     return crash
 
-def test_input_version():
+
+def test_input_version(input):
 
     print("test_input_version")
 
@@ -44,27 +45,18 @@ def test_input_version():
 
     while not bug:
         
-        file1 = open('testinput.img', 'rb')
-        file2 = open('newinput.img', 'wb+')
+        file = open('newinput.img', 'wb+')
 
-        byte = file1.read(1)
-        file2.write(byte)
-
-        a = random.randint(0, 10000)
+        a = random.randint(0, 1000)
         byte_to_write = a.to_bytes(2,'little')
-        count = 0
-        while byte:
-            if(count == 1):
-                byte = file1.read(1)
-                file2.write(byte_to_write)
-                count += 1 
-            else:
-                byte = file1.read(1)
-                file2.write(byte) 
-                count += 1
 
-        file1.close()
-        file2.close()
+        for i in range(0, 9):
+            if(i == 1):
+                file.write(byte_to_write)
+            else:
+                file.write(input[i])
+
+        file.close()
 
         
         if lunch_process('bad_version.img'):
@@ -74,8 +66,72 @@ def test_input_version():
             print("Test has reached the timeout")
             break
 
+def test_width_heigh(input):
 
-def test_comment_size():
+    print("test_width_heigh")
+
+    timeout = time.time() + 60
+    bug = 0
+    count = 0
+    while not bug:
+        
+        file = open('newinput.img', 'wb+')
+
+        byte_to_write = count.to_bytes(4,byteorder='big')
+
+        for i in range(0, 9):
+            if(i == 3):
+                file.write(b'\x02')
+                file.write(byte_to_write)
+                file.write(byte_to_write)
+            else:
+                file.write(input[i])
+
+        file.close()
+        count += 1
+
+        
+        if lunch_process('bad_width_heigh.img'):
+            bug = 1
+
+        if time.time() > timeout:
+            print("Test has reached the timeout")
+            break
+
+def test_width(input):
+
+    print("test_width")
+
+    timeout = time.time() + 60
+    bug = 0
+    count = 0
+    while not bug:
+        
+        file = open('newinput.img', 'wb+')
+
+        byte_to_write = count.to_bytes(4,byteorder='big')
+
+        for i in range(0, 9):
+            if(i == 3):
+                file.write(b'\x02')
+                file.write(b'\x02\x00\x00\x00')
+                file.write(byte_to_write)
+            else:
+                file.write(input[i])
+
+        file.close()
+        count += 1
+
+        
+        if lunch_process('bad_width.img'):
+            bug = 1
+
+        if time.time() > timeout:
+            print("Test has reached the timeout")
+            break
+
+
+def test_comment_size(input):
 
     print("test_comment_size")
 
@@ -84,29 +140,21 @@ def test_comment_size():
 
     while not bug:
 
-        file1 = open('testinput.img', 'rb')
-        file2 = open('newinput.img', 'wb+')
-
-        byte = file1.read(1)
-        file2.write(byte)
+        file = open('newinput.img', 'wb+')
 
         a = random.randint(0, 10000)
         random_comment = random_string(a)
         hex_string = str.encode(random_comment)
-        while byte:
-            if(byte == b'\x0c'):
-                file2.write(byte)
-                byte = file1.read(1)
-                file2.write(hex_string)
-                while byte != b'\x00':
-                    byte = file1.read(1)
-                file2.write(byte)
-            else:
-                byte = file1.read(1)
-                file2.write(byte)
 
-        file1.close()
-        file2.close()
+        for i in range(0, 9):
+            if(i == 6):
+                file.write(b'\x0c')
+                file.write(hex_string)
+                file.write(b'\x00')
+            else:
+                file.write(input[i])
+
+        file.close()
 
         if(lunch_process('bad_comment_size.img')):
             bug = 1
@@ -116,41 +164,30 @@ def test_comment_size():
             break
 
 
-def test_name_size():
+def test_name_size(input):
 
     print("test_name_size")
 
-    timeout = time.time() + 60
+    timeout = time.time() + 30
     bug = 0
 
     while not bug:
 
-        file1 = open('testinput.img', 'rb')
-
-        file2 = open('newinput.img', 'wb+')
-
-        byte = file1.read(1)
-        file2.write(byte)
+        file = open('newinput.img', 'wb+')
 
         a = random.randint(0, 1000)
         random_comment = random_string(a)
         hex_string = str.encode(random_comment)
-        count = 0
-        while byte:
-            if(byte == b'\x01' and count == 0):
-                file2.write(byte)
-                byte = file1.read(1)
-                file2.write(hex_string)
-                while byte != b'\x00':
-                    byte = file1.read(1)
-                file2.write(byte)
-                count += 1
-            else:
-                byte = file1.read(1)
-                file2.write(byte)
 
-        file1.close()
-        file2.close()
+        for i in range(0, 9):
+            if(i == 2):
+                file.write(b'\x01')
+                file.write(hex_string)
+                file.write(b'\x00')
+            else:
+                file.write(input[i])
+
+        file.close()
 
         if(lunch_process('bad_name_size.img')):
             bug = 1
@@ -160,46 +197,7 @@ def test_name_size():
             break
 
 
-def test_color_table():
-
-    print("test_color_table")
-
-    timeout = time.time() + 60
-    bug = 0
-
-    while not bug:
-
-        file1 = open('testinput.img', 'rb')
-
-        file2 = open('newinput.img', 'wb+')
-
-        byte = file1.read(1)
-        file2.write(byte)
-
-        a = random.randint(0, 50)
-        random_comment = random_string(a*8)
-        hex_string = str.encode(random_comment)
-        while byte:
-            if(byte == b'\x0b'):
-                byte = file1.read(1)
-                file2.write(hex_string)
-                byte = file1.read(8)
-            else:
-                byte = file1.read(1)
-                file2.write(byte)
-
-        file1.close()
-        file2.close()
-
-
-        if(lunch_process('bad_color_table.img')):
-            bug = 1
-
-        if time.time() > timeout:
-            print("Test has reached the timeout")
-            break
-
-def test_image_table():
+def test_image_table(input):
 
     print("test_image_table")
 
@@ -208,32 +206,22 @@ def test_image_table():
 
     while not bug:
 
-        file1 = open('testinput.img', 'rb')
-
-        file2 = open('newinput.img', 'wb+')
-
-        byte = file1.read(1)
-        file2.write(byte)
+        file = open('newinput.img', 'wb+')
 
         random_comment_1 = random_string(2)
         random_comment_2 = random_string(2)
         hex_string_1 = str.encode(random_comment_1)
         hex_string_2 = str.encode(random_comment_2)
-        count = 0
-        while byte:
-            if(count == 40):
-                file2.write(hex_string_1)
-                file2.write(hex_string_2)
-                count += 1
-            elif(count < 40):
-                byte = file1.read(1)
-                file2.write(byte)
-                count += 1
-            else:
-                byte = file1.read(1)
 
-        file1.close()
-        file2.close()
+        for i in range(0, 9):
+            if(i == 7):
+                file.write(hex_string_1)
+            elif(i == 8):
+                file.write(hex_string_2)
+            else:
+                file.write(input[i])
+
+        file.close()
 
 
         if(lunch_process('bad_image_table.img')):
@@ -243,13 +231,23 @@ def test_image_table():
             print("Test has reached the timeout")
             break
 
+def testinput():
 
-def test_no_io():
-    test_input_version() # works and return the failure
-    test_comment_size()  # works and return the failure
-    test_color_table()   # works and return the failure
-    test_name_size()     # works and return the failure
-    test_image_table()
+    a = [b'\xab\xcd', b'd\x00', b'\x01\x52\x61\x6d\x69\x6e\x00', b'\x02\x02\x00\x00\x00\x02\x00\x00\x00', b'\x0a\x02\x00\x00\x00', b'\x0b\x00\x00\x00\x00\xff\xff\xff\x00', b'\x0c\x48\x65\x6c\x6c\x6f\x00', b'\x00\x01', b'\x01\x00']
+    return a
 
 
-test_no_io()
+
+def fuzzer():
+
+    input = testinput()
+
+    test_width_heigh(input)
+    test_width(input)
+    test_input_version(input) # works and return the failure
+    test_comment_size(input)  # works and return the failure
+    test_image_table(input)   # works and return the failure
+    test_name_size(input)     # works and return the failure
+
+
+fuzzer()
